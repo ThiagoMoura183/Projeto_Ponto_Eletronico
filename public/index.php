2bb5e3c32547be236a82e2b55f9df981dc8a97fc
Thiago Moura <thiago.moura2000@hotmail.com>
2022-10-25 18:16:52 -0300

    Criação de tela de login - Sem sync/validação

--- public/index.php
+++ public/index.php
@@ -1,17 +1,5 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-$user = new User(['name' => 'Thiago', 'email' => 'thiago@teste.com']);
-
-print_r(User::get(['name' => 'Chaves'], 'id, name, email'));
-echo '<br>';
-
-print_r(User::get([], 'id, name'));
-echo '<br>';
-
-foreach (User::get([], 'id, name') as $user) {
-    echo $user->name;
-    echo '<br>';
-}
+// require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
--- src/views/login.php
+++ src/views/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt_BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,7 +11,33 @@
     <link rel="stylesheet" href="assets/css/login.css">
     <title>In 'n Out</title>
 </head>
+
 <body>
-    <div>Login</div> 
+    <form class="form-login" action="#" method="POST">
+        <div class="login-card card">
+            <div class="card-header">
+                <i class="icofont-travelling mr-2"></i>
+                <span class="font-weight-light">In</span>
+                <span class="font-weight-bold mx-2">'N</span>
+                <span class="font-weight-light">Out</span>
+                <i class="icofont-runner-alt-1 ml-2"></i>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input class="form-control" type="email" name="email" id="email" placeholder="Digite seu e-mail" autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input class="form-control" type="password" name="password" id="password" placeholder="Digite sua senha">
+                </div>
+            </div>
+
+            <div class="card-footer">
+                <button class="btn btn-lg btn-primary">Entrar</button>
+            </div>
+        </div>
+    </form>
 </body>
+
 </html>
