2acdffd1ce8563b248ae40febb50c22255520519
Thiago Moura <thiago.moura2000@hotmail.com>
2022-10-26 09:28:45 -0300

    Criação da model Login e criada prévia de validação de e-mail e senha de usuário - SEM DADOS VIA POST (FORMULÁRIO)

--- public/index.php
+++ public/index.php
@@ -2,4 +2,19 @@
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 // require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-require_once(dirname(__FILE__, 2) . '/src/views/login.php');
+
+//--------- Mesmas coisas ---------//
+// require_once(dirname(__FILE__, 2) . '/src/views/login.php'); // O '2' serve para acessar a 2º pasta superior sobre o arquivo que estamos.
+// require_once(VIEW_PATH . '/login.php');
+//---------------------------------//
+
+require_once(MODEL_PATH . '/Login.php');
+
+$login = new Login(['email' => 'admin@cod3r.com.br', 'password' => 'a']);
+
+try {
+    $login->checkLogin();
+    echo "Usuário logado com sucesso";
+} catch (Exception $e) {
+    echo "Problema no Login!";
+}
