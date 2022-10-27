19a15129ba7baf15084dc3247c9e2722af2193c4
Thiago Moura <thiago.moura2000@hotmail.com>
2022-10-27 17:37:59 -0300

    Ajuste de CSS para template (Header)

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -12,7 +12,7 @@
     <title>In 'n Out</title>
 </head>
 
-<body>
+<body class="hide-sidebar">
     <header class="header">
         <div class="logo">
             <i class="icofont-travelling mr-2"></i>
@@ -24,7 +24,20 @@
         <div class="menu-toggle mx-3">
             <i class="icofont-navigation-menu"></i>
         </div>
-        <div class="spacer">
-            
+        <div class="spacer"></div>
+        <div class="dropdown">
+            <div class="dropdown-button">
+                <span class="ml-3">Usuário Exemplo</span>
+                <i class="icofont-simple-down mx-2"></i>
+            </div>
+            <div class="dropdown-content">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="logout.php">
+                            <i class="icofont-logout mr-2"></i>Sair
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </header>
