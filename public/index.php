a11c74ba469eefbf5e0b1948ff641953c7a7ef11
Thiago Moura <thiago.moura2000@hotmail.com>
2022-10-27 12:20:19 -0300

    Preparação de redirecionamentos na página index utilizando REQUEST URI

--- public/index.php
+++ public/index.php
@@ -6,8 +6,19 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
 //--------- Mesmas coisas ---------//
 // require_once(dirname(__FILE__, 2) . '/src/views/login.php'); // O '2' serve para acessar a 2º pasta superior sobre o arquivo que estamos.
-require_once(CONTROLLER_PATH . '/login.php');
+// require_once(CONTROLLER_PATH . '/login.php');
 //---------------------------------//
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+); // O Request pegará o caminho que está caindo. Já o URLDecoder vai deixar no padrão URL.
+
+//Essa é uma validação para que, caso o arquivo seja / ou vazio, caia diretamente no login.
+if ($uri === '/' || $uri === '' || $uri === '/index.php') {
+    $uri = '/login.php';
+}
+
+require_once(CONTROLLER_PATH . "/{$uri}");
 
 //--------- Teste para saber se o login está sendo validado ---------//
 // require_once(MODEL_PATH . '/Login.php');
