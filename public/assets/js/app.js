791383c29b03ce9fa7189ef99c2a9701544f6439
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-04 12:23:18 -0300

    Tratativas de datas para automatização do bater ponto + cálculo de horário de saída

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,3 +5,28 @@
         body.classList.toggle('hide-sidebar')
     }
 })()
+
+
+// Adicionar zero à esquerda do horário
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // Adicionar zero à esquerda do horário
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+      startTime()
+    }, 500);
+  }
+
+  startTime();
