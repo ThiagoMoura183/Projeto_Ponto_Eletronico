4ca495651b4dfc571a95230cc6ae3696fae1412f
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-07 13:48:56 -0300

    Atualização para adicionar funcionalidade de EDIÇÃO dos usuários

--- src/controllers/save_user.php
+++ src/controllers/save_user.php
@@ -4,21 +4,37 @@ session_start();
 requireValidSession();
 
 $exception = null;
+$userData = [];
 
-if (count($_POST) > 0) {
+// Funcionalidade para já exibir as informações cadastradas! - Quando clicado em editar
+if (count($_POST) === 0 && isset($_GET['update'])) {
+    $user = User::getOne(['id' => $_GET['update']]);
+    $userData = $user->getValues();
+    $userData['password'] = null;
+} elseif (count($_POST) > 0) {
     try {
-        $newUser = new User($_POST);
-        $newUser->insert();
-        addSuccessMsg('Usuário cadastrado com sucesso!');
+        $dbUser = new User($_POST);
+        if ($dbUser->id) {
+            $dbUser->update();
+            addSuccessMsg('Usuário alterado com sucesso!');
+            header('Location: users.php'); //Redirecionar para a listagem de usuários
+            exit;
+        } else {
+            $dbUser->insert();
+            addSuccessMsg('Usuário cadastrado com sucesso!');
+        }
         $_POST = [];
     } catch (Exception $e) {
         $exception = $e;
+    } finally {
+        $userData = $_POST;
     }
 }
 
 loadTemplateView(
     'save_user',
-    [
-        'exception' => $exception
-    ]
+    $userData +
+        [
+            'exception' => $exception
+        ]
 );
