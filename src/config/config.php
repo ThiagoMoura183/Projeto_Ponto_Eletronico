a3705141b173af5567faa975cae8537a02d425e3
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-07 13:57:48 -0300

    Conclusão do projeto!

--- src/config/config.php
+++ src/config/config.php
@@ -6,7 +6,6 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 // Constantes gerais
 define('DAILY_TIME', 60 * 60 * 8);
 
-
 // Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
